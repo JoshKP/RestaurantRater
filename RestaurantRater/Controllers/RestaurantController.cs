bf3248b3c8659514039fb5949721ea525086cd86
Josh <joshpiersimoni@gmail.com>
2020-08-25 15:10:25 -0400

    built out get by id

---
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -0,0 +1,52 @@
+﻿using RestaurantRater.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RestaurantRater.Controllers
+{
+    public class RestaurantController : ApiController
+    {
+        private readonly RestaurantDbContext _context = new RestaurantDbContext();
+        // Create (POST)
+        [HttpPost] //<--Indicates method is going to be a POST
+        public async Task<IHttpActionResult> PostRestaurant(Restaurant model)
+        {
+            if (model is null)
+            {
+                return BadRequest("Your request body cannot be empty.");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Restaurants.Add(model);
+                await _context.SaveChangesAsync();
+
+                return Ok("You created a restaurant and it was saved!");
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        // Read (GET)
+        // -Get by ID
+
+        // -Get all
+        [HttpGet]
+        public async Task<IHttpActionResult> GetAll()
+        {
+            List<Restaurant> restaurants = await _context.Restaurants.ToListAsync();
+            return Ok(restaurants);
+        }
+
+
+        // Update (PUT)
+
+        // Delete (DELETE)
+
+    }
+}
--- RestaurantRater/Models/Restaurant.cs
+++ RestaurantRater/Models/Restaurant.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace RestaurantRater.Models
 {
+    // Restaurant Entity (The class that gets stored in the database)
     public class Restaurant
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public double Rating { get; set; }
+        public bool IsRecommended        // "public bool IsRecommended => Rating > 3.5;"
+        {
+            get
+            {
+                return Rating > 3.5;
+            }
+        }   
+        
+
     }
 }
